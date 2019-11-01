13a69403b9d26ad17f53d6e430a09a2464c81d0b
Initial Commit <>
2019-11-01 15:55:00 -0600

    Basic sign in and set up linter airbnb

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import ReactDOM from 'react-dom';
-import { Tasks } from '../api/tasks.js';
- 
-import Task from './Task.js';
- 
+import { Tasks } from '../api/tasks';
+
+import Task from './Task';
+import AccountsUIWrapper from './AccountsUIWrapper';
+
 // App component - represents the whole app
 class App extends Component {
   constructor(props) {
@@ -17,15 +18,15 @@ class App extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
- 
+
     // Find the text field via the React ref
     const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
- 
+
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
     });
- 
+
     // Clear form
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
@@ -35,7 +36,7 @@ class App extends Component {
       hideCompleted: !this.state.hideCompleted,
     });
   }
- 
+
   renderTasks() {
     let filteredTasks = this.props.tasks;
     if (this.state.hideCompleted) {
@@ -45,14 +46,14 @@ class App extends Component {
       <Task key={task._id} task={task} />
     ));
   }
-  
+
   render() {
     return (
       <div className="container">
         <header>
-        <h1>Todo List ({this.props.incompleteCount})</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
-        <label className="hide-completed">
+          <label className="hide-completed">
             <input
               type="checkbox"
               readOnly
@@ -61,6 +62,8 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
+
+          <AccountsUIWrapper />
  
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
@@ -77,7 +80,7 @@ class App extends Component {
     );
   }
 }
- 
+
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
